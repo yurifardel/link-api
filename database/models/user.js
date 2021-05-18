92d37119af2afb3cc2f7fd86050d1af8a5c0899c
yurifardel <yrnascimento@outlook.com>
2021-05-18 01:59:23 -0300

    created user schema

---
+++ database/models/user.js
@@ -0,0 +1,31 @@
+const mongo = require("../mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongo.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+const User = mongo.model("Users", UserSchema);
+
+module.exports = User;
