e3c85859c49aa65eaef8fe71b731b0cb1eca2e33
yurifardel <yrnascimento@outlook.com>
2021-05-19 02:14:50 -0300

    implementing jwt

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,18 +1,31 @@
 const User = require("../../database/models/user");
+// const {} = require('../helpers/msg')
+const {
+  generateJwt,
+  generateRefreshJwt,
+  verifyRefreshJwt,
+  getTokenFromHeaders,
+} = require("../helpers/jwt");
 
 module.exports = {
   async index(req, res) {
-    const { email } = req.body;
-
     try {
+      const { email, password } = req.body;
+
       if (await User.findOne({ email })) {
         return res.json({ status: 400, error: "user already exists" });
       }
 
-      const user = User.create(req.body);
+      const user = User.create({ email, password });
 
       const promise = user.then((data) => {
-        return res.json(data);
+        const token = generateJwt({ id: data.id });
+        const refreshToken = generateRefreshJwt({
+          id: data.id,
+          version: data.jwtVersion,
+        });
+
+        return res.json({ data, token, refreshToken });
       });
 
       return promise;
