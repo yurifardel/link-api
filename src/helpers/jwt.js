a630f132ac28fa910080eb7724c1b6ecc4119c45
yurifardel <yrnascimento@outlook.com>
2021-05-19 02:16:46 -0300

    created config jwt

---
+++ src/helpers/jwt.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+
+const TokenPrivateKey = process.env.JWT_TOKEN_PRIVATE_KEY;
+const RefreshTokenPrivateKey = process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY;
+
+const option = { expiresIn: "30 minutes" };
+const refreshOptions = { expiresIn: "30 days" };
+
+const generateJwt = (payload) => {
+  return jwt.sign(payload, TokenPrivateKey, option);
+};
+
+const generateRefreshJwt = (payload) => {
+  return jwt.sign(payload, RefreshTokenPrivateKey, refreshOptions);
+};
+
+const verifyJwt = (token) => {
+  return jwt.verify(token, TokenPrivateKey);
+};
+
+const verifyRefreshJwt = (token) => {
+  return jwt.verify(token, RefreshTokenPrivateKey);
+};
+
+const getTokenFromHeaders = (headers) => {
+  let token = headers["authorization"];
+  return (token = token ? token.slice(7, token.length) : null);
+};
+
+module.exports = {
+  generateJwt,
+  generateRefreshJwt,
+  verifyJwt,
+  verifyRefreshJwt,
+  getTokenFromHeaders,
+};
