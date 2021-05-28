cee251227b3edf498ed1ce1ddebd8c373887be68
yurifardel <yrnascimento@outlook.com>
2021-05-28 00:53:58 -0300

    globally executing the http response methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const response = require("./middlewares/response");
 
 const AccountController = require("../src/controllers/AccountController");
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(response);
 
 app.use("/auth", AccountController);
 
