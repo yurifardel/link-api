fda5ac6f34aba3668f22eae2ab9b60ae06b0e2e8
yurifardel <yrnascimento@outlook.com>
2021-06-09 00:39:48 -0300

    config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const response = require("./middlewares/response");
 const checkJwt = require("./middlewares/jwt");
 
 const AccountController = require("../src/controllers/AccountController");
+const LinkController = require("../src/controllers/LinkController");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", AccountController);
+app.use("/", LinkController);
 
 app.listen(3000, () => {
   console.log("express listening on port 3000");
